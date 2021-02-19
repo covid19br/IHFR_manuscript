b82232a560dda6eb9fe0a31ae6799eda9b231ca5
saramortara <saramortara@gmail.com>
2021-02-19 16:21:27 -0300

    fixing small bugs in the main plot script

--- scripts/03_plotting_IHFR_glm.R
+++ scripts/03_plotting_IHFR_glm.R
@@ -24,13 +24,13 @@ hosp_week <- read_csv("data/processed/hospitalizados_srag_week_2020_09_28.csv")
 
 # Converting age group for the legend
 class_new <- c("0-19", "20-39", "40-59", "60 or over")
-names(class_new) <- unique(df_covid_glm$age_clas)
+names(class_new) <- unique(df$age_clas)
 
-df_covid_glm$age_clas <- str_replace_all(df_covid_glm$age_clas, class_new)
-df_srag_glm$age_clas <- str_replace_all(df_srag_glm$age_clas, class_new)
+df$age_clas <- str_replace_all(df$age_clas, class_new)
+df$age_clas <- str_replace_all(df$age_clas, class_new)
 
 # Ordering states by letality
-uf_ordered <- df_covid_glm %>% filter(age_clas == "60 or over") %>%
+uf_ordered <- df %>% filter(age_clas == "60 or over") %>%
   group_by(sg_uf) %>%
   summarise(max = max(fit)) %>%
   arrange(desc(max)) %>%
@@ -47,22 +47,24 @@ p1 <- ggplot(df, aes(x = week, y = let_obs, group = age_clas)) +
   geom_ribbon(aes(ymin = lwr, ymax = upr, fill = age_clas), alpha = 0.2) +
   scale_fill_manual(values = pal, name = "Age class") +
   theme_classic() +
-  theme(legend.position = c(0.88, 0.03),
-        legend.direction = "vertical") +
   xlab("Week") +
   ylab("In-hospital lethality") +
-  theme(strip.background = element_blank())
+  theme(strip.background = element_blank(),
+        legend.position = c(0.95, 0.6),
+        legend.title = element_text(size = 7),
+        legend.text = element_text(size = 7),
+        legend.direction = "horizontal")
 
-p1
+# Extracting legend from p1
+legenda <- get_legend(p1)
 
-
-# 2. Function to make one plot per state ---------------------------------------
+# 2. Plotting for each state separately ----------------------------------------
 
 # Binding IHFR data and hospitalizations
 df_hosp <- left_join(df, hosp_week, by = c("week", "sg_uf")) %>%
   # Creating column w/ factor for the second axis
   group_by(sg_uf) %>%
-  mutate(fac = ceiling(max(hosp)/100)*100, bar = hosp/fac)
+  mutate(fac = ceiling(max(hosp)/100) * 100, bar = hosp/fac)
 
 # Making plots by UF using accessory function
 plot_ufs_fac(df = df_hosp, strip_size = 8, strip_color = "white")
@@ -71,10 +73,11 @@ plot_ufs_fac(df = df_hosp, strip_size = 8, strip_color = "white")
 # 3. Reading plots on disk and arranging in a panel ----------------------------
 # This is the only way to arrange graphics w/ two y-axis varying in scale
 
+# 3.1. Drawing the plots -------------------------------------------------------
 # File paths to figure
 filename <- list.files("figs/ufs", pattern = 'covid', full.names = TRUE)
 
-# Exact UF acronym
+# Getting the exact UF acronym with respect to the filename
 uf <- sapply(strsplit(filename, "covid_"), function(x) x[2]) %>%
   gsub(".png", "", .)
 
@@ -89,17 +92,9 @@ for (i in 1:length(filename)) {
   assign(uf[i], plot_list[[i]], envir = .GlobalEnv)
 }
 
-# Extracting legend from the first plot
-ex <- p1 +
-  theme(legend.position = c(0.95, 0.6),
-        legend.title = element_text(size = 7),
-        legend.text = element_text(size = 7),
-        legend.direction = "horizontal")
 
-legenda <- get_legend(ex)
-
-# 3.1. Arranging and saving the main plot --------------------------------------
-tiff("figs/main_figure.tiff",
+# 3.2. Arranging and saving the main plot --------------------------------------
+tiff("figs/figure01.tiff",
      width = 180, height = 210, units = "mm", res = 300)
 ggarrange(
   # by hand using ordered UF
