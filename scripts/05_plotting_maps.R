681fa0d8d6c232fd35a1f99e03129c433af2a1ad
portellatp <50343841+portellatp@users.noreply.github.com>
2021-12-16 09:31:45 -0300

    adicionando OR e IHFR 80+ nos mapas

--- scripts/05_plotting_maps.R
+++ scripts/05_plotting_maps.R
@@ -10,6 +10,16 @@ library(GGally)
 
 # Reading data
 med_int_shape <- read_sf("data/shapefile/hospital_indicators.shp")
+odds<-read_csv("data/processed/odds_ratio.csv")
+
+odds<- odds %>% filter (age_clas=="age_80") %>%
+  select(sg_uf, fit_max, or) %>%
+  as.data.frame()
+
+
+med_int_shape<-med_int_shape[,-5]
+
+med_int_shape<-full_join(med_int_shape, odds, by="sg_uf")
 
 # General parameters -----------------------------------------------------------
 
@@ -41,78 +51,85 @@ med_int_shape$ymod[med_int_shape$sg_uf == "SE"] <- -0.3
 med_int_shape$ymod[med_int_shape$sg_uf == "AL"] <- -0.1
 med_int_shape$ymod[med_int_shape$sg_uf == "RJ"] <- -0.2
 
-med_int_shape$col <- ifelse(med_int_shape$sg_uf %in% c("AM", "RO", "PA"),
-                            "white", "black")
+#med_int_shape$col <- ifelse(med_int_shape$sg_uf %in% c("AM", "RO", "PA"),
+#                           "white", "black")
 #med_int_shape$label[med_int_shape$sg_uf %in% c("DF", "ES", "RJ", "SE", "AL", "PE", "PB", "RN", "SC", "GO", "PI", "AC")] <- NA
 
 # 1. Peak in-hospital mortality map --------------------------------------------
 
 fit_map <- tm_shape(med_int_shape) +
   tm_borders() +
-  tm_text("sg_uf", size = 0.45,
-          remove.overlap = TRUE,
-          xmod = "xmod", ymod = "ymod", col = "col") +
-  tm_fill("IHFR",
-          title = "Peak of in-hospital mortality",
-          style = "cont",
-          colorNA = "gray") +
+  tm_fill("fit_max",
+          title = "Peak of IHM 80+", style = "cont") +
   tm_style("col_blind") +
-  tm_layout(title = "A.",
-            inner.margins = c(0, 0, 0, 0.1)) +
-  tm_text("") +
+  tm_layout(title = "A.") +
   my_layout
 
 fit_map
 
-# 2. Intensivists map ----------------------------------------------------------
+# 2. Odds Ratio  --------------------------------------------
+
+ods_map <- tm_shape(med_int_shape) +
+  tm_borders() +
+  tm_fill("or",
+          title = "Odds ratio IHM 80+", style = "cont") +
+  tm_style("col_blind") +
+  tm_layout(title = "B.") +
+  my_layout
+
+ods_map
+
+# 3. Intensivists map ----------------------------------------------------------
 med_int <- tm_shape(med_int_shape) +
   tm_borders() +
   tm_fill("pMI",
           title = "Intensivists", style = "cont") +
   tm_style("col_blind") +
-  tm_layout(title = "B.") +
-  my_layout
+ tm_layout(title = "C.") +
+ my_layout
 
 med_int
 
-# 3. Physicians map ------------------------------------------------------------
+# 4. Physicians map ------------------------------------------------------------
 med <- tm_shape(med_int_shape) +
   tm_borders() +
   tm_fill("pmed",
           title = "Physicians", style = "cont") +
   tm_style("col_blind") +
-  tm_layout(title = "C.") +
+  tm_layout(title = "D.") +
   my_layout
 
 med
 
-# 4. ICU map -------------------------------------------------------------------
+# 5. ICU map -------------------------------------------------------------------
 uti <- tm_shape(med_int_shape) +
   tm_borders() +
   tm_fill("UTI",
           title = "ICU",
           style = "cont") +
   tm_style("col_blind") +
-  tm_layout(title = "D.") +
+  tm_layout(title = "E.") +
   my_layout
 
 uti
 
 
-# 5. SUS dependent--------------------------------------------------------------
+# 6. SUS dependent--------------------------------------------------------------
 sus <- tm_shape(med_int_shape) +
   tm_borders() +
   tm_fill("SUS",
           title = "% SUS-dependent",
           style = "cont") +
   tm_style("col_blind") +
-  tm_layout(title = "E.") +
+  tm_layout(title = "F.") +
   my_layout
 
 sus
 
 # 6. Arrange and save -------------------------------------------------------------
-a <- tmap_arrange(fit_map, med_int, med, uti, sus)
+a <- tmap_arrange(fit_map, ods_map, med, med_int, uti, sus)
+
+a
 #a
 tmap_save(a, "figs/figure_02.tiff", width = 200, height = 180, dpi = 300, units = "mm")
 
